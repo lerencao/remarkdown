622d40dada6517ff4a045690526935fb9872cb44
Cao Jiafeng <funfriendcjf@gmail.com>
2014-09-04 22:55:19 +0800

    update blocks and add inline elements definition

--- src/main/scala/startman/remarkdown/Block.scala
+++ src/main/scala/startman/remarkdown/Block.scala
@@ -6,21 +6,21 @@ package startman.remarkdown
  * @since 06/23/2014
  */
 sealed abstract class Block
-
 case object Empty extends Block
-
-case class Heading(level: Int, raw: String) extends Block
-
 case object HorizontalRule extends Block
+case class Heading(level: Int, var contents: Seq[Inline]) extends Block
+case class Paragraph(var contents: Seq[Inline]) extends Block
+case class CodeBlock(contents: String) extends Block
+case class BlockQuote(var contents: Seq[Block]) extends Block
+case class List(kind: Boolean, var contents: Seq[ListItem]) extends Block
 
-case class CodeBlock(raw: String) extends Block
+case class ListItem(var contents: Seq[Block])
 
-case class BlockQuote(raw: String) extends Block
 
-case class ListItem(raw: String)
-
-case class List(kind: Boolean, ls: Seq[ListItem]) extends Block
-
-case class Nested(raw: String)
-
-case class Paragraph(raw: String) extends Block
+sealed abstract class Inline
+case class PlainText(contents: String) extends Inline
+case class Strong(contents: String) extends Inline
+case class Underscore(contents: String) extends Inline
+case class Italics(contents: String) extends Inline
+case class Link(contents: String, url: String) extends Inline
+case class InlineCode(contents: String) extends Inline
